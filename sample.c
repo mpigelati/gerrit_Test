3781e7da565f00bc7739ba2f4f6525dee8e955b6
mounika <nanivemunuri@gmail.com>
2018-06-27 15:43:43 +0530

    modified

--- sample.c
+++ sample.c
@@ -3,9 +3,12 @@
 void main()
 
 {
-
+	char s1[100]="innomionds hyderabad",s2[100],*ptr;
+	ptr=my_strcpy(s2,s1);
+	printf("the copy string :%s\n",ptr);
 	fun_sample();
 	printf("hellow");
+	strcpy()
 }
 
 
